5272113f25f159ecc034be02fb12f98a2b309a31
YorkGrizzly <YorkGrizzly@github.com>
2019-06-08 14:58:51 +0800

    6/8 not_valid_maze判斷

--- pattern_run.sv
+++ pattern_run.sv
@@ -31,7 +31,7 @@ integer output_file_x;
 integer output_file_y;
 integer f;
 integer PAT_NUM = 100;
-integer patcount;
+integer patcount = 0;
 
 always	#(CYCLE/2.0) clk = ~clk;
 
@@ -58,14 +58,24 @@ end
 
 always@(negedge clk) begin
   if(out_valid===1) begin
-    outcount = outcount + 1;
-    f = $fscanf(output_file_x, "%d", gold_x);
-    f = $fscanf(output_file_y, "%d", gold_y);
-    if(out_x!==gold_x||out_y!==gold_y) begin  //wrong output
-      $display("---------------------------------------------\n\n");
-      $display("   correct answer x: %d, y: %d\n   your answer  out_x: %d, out_y: %d  at %t, pattern number %d\n\n", gold_x,   gold_y, out_x, out_y, $time, patcount + 1);
-      $display("---------------------------------------------");
-      repeat(4)@(negedge clk); $finish;
+    if(goldnum===0) begin //not valid maze
+      if(maze_not_valid!==1) begin
+        $display("--------------------------------------------\n\n");
+        $display("  The maze is not valid!   at %t, pattern number %d\n\n", $time, patcount + 1);
+        $display("--------------------------------------------");
+        repeat(4)@(negedge clk); $finish;
+      end
+    end
+    else begin  //valid maze
+      outcount = outcount + 1;
+      f = $fscanf(output_file_x, "%d", gold_x);
+      f = $fscanf(output_file_y, "%d", gold_y);
+      if(out_x!==gold_x||out_y!==gold_y) begin  //wrong output
+        $display("---------------------------------------------\n\n");
+        $display("   correct answer x: %d, y: %d\n   your answer  out_x: %d, out_y: %d  at %t, pattern  number %d\n\n", gold_x,   gold_y, out_x, out_y, $time, patcount + 1);
+        $display("---------------------------------------------");
+        repeat(4)@(negedge clk); $finish;
+      end
     end
   end
   else if(out_valid===0 && outcount!== 0) begin //wrong path length
@@ -81,7 +91,7 @@ always@(negedge clk) begin
       repeat(4)@(negedge clk); $finish;        
     end
     else outcount = 0;
-  end
+  end  
 end
 
 always@(negedge clk) begin
@@ -104,6 +114,7 @@ task give_input; begin
     @(negedge clk);
   end
   in_valid = 0;
+  maze = 0;
 end endtask
 
 task reset_check; begin
